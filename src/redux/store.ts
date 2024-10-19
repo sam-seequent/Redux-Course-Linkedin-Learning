5fd2cc9231eb4dff59535cebabcde1cf9f47ce8b
Chiomadans <oluchukwu.alooba.com>
2024-10-19 12:17:42 +0200

    ch1_vd3

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+const preloadedState = {
+  cart: {
+    items: [{ id: 1, name: 'Shoes', price: 50 }],
+    totalPrice: 50,
+  },
+};
+
 export const store = configureStore({
   reducer: {},
+  devTools: process.env.NODE_ENV !== 'production',
+  preloadedState,
 });
-  
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
