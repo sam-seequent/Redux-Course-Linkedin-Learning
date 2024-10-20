8df3345d6cd5e2e13017323c2630028f0563d080
Chiomadans <oluchukwu.alooba.com>
2024-10-20 09:30:17 +0200

    ch2_vd2

--- src/redux/reducers/cartReducer.ts
+++ src/redux/reducers/cartReducer.ts
@@ -1,3 +1,4 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 export interface CartItem {
   id: number
   price: number;
@@ -17,3 +18,24 @@ const initialState: CartState = {
   items: [],
   totalPrice: 0,
 };
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart(state, action: PayloadAction<CartItem>) {
+      state.items.push(action.payload);
+      state.totalPrice += action.payload.price;
+    },
+    removeFromCart(state, action: PayloadAction<number>) {
+      const item = state.items.find(item => item.id === action.payload);
+      if (item) {
+        state.totalPrice -= item.price;
+        state.items = state.items.filter(item => item.id !== action.payload);
+      }
+    }
+  },
+});
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+export default cartSlice.reducer;
