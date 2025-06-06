8e5122a07795a967eda4fd7aba5fe012d9ebebb1
sam-seequent <sam.laquerre@bentley.com>
2025-06-06 15:58:38 -0600

    Dependency versions updated

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
 ReactDOM.render(
+  /* This provider component is what allows the store to be connected to the rest of the react application */
   <Provider store={store}>
     <App />
   </Provider>,
